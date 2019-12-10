56498aa59c6ce1a1b952d062d381abb66436a1f3
Evgeny Karasev <gnomskg@gmail.com>
2019-12-10 16:31:36 +0300

    Add rest api for working with service and loading map congis

--- routes/layers.js
+++ routes/layers.js
@@ -1,17 +1,40 @@
-const express = require("express");
-const router = express.Router();
+var express = require("express");
+var router = express.Router();
 
-/* GET layers listing. */
-router.get("/", function(req, res) {
-  res.send(200);
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.json(req.db.getLayers());
 });
 
-router.get("/scheme", function(req, res) {
-  res.send(200);
+router.get("/configs/:id", function(req, res, next) {
+  const { id } = req.params;
+  const config = req.db.getLayerConfig(id);
+  if (config) {
+    res.json(config);
+  } else {
+    res.send(404);
+  }
 });
 
-router.get("/geodata", function(req, res) {
-  res.send(200);
+router.post("/configs", function(req, res, next) {
+  const layer = req.body;
+  const result = req.db.createLayer(layer);
+  if (result) {
+    res.send(200);
+  } else {
+    res.send(400);
+  }
+});
+
+router.put("/configs/:id", function(req, res, next) {
+  const { id } = req.params;
+  const layer = req.body;
+  const result = req.db.updateLayer(id, layer);
+  if (result) {
+    res.send(200);
+  } else {
+    res.send(400);
+  }
 });
 
 module.exports = router;
